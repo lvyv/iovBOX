b45f6a78e7bae6936d54a0a683dab5c7dc81c04d
lvyv <26896225@qq.com>
2018-01-07 17:35:29 +0800

    changed

--- broker.js
+++ broker.js
@@ -27,39 +27,38 @@ function handle_request(req, res) {
     if (pathname.charAt(pathname.length-1)==="/"){
         pathname+='broker.htm';
     }
-    fs.exists(pathname,function(exists){
-        if(exists){
-            switch(path.extname(pathname)){
-                case ".html":
-				case ".htm":
-                    res.writeHead(200, {"Content-Type": "text/html"});
-                    break;
-                case ".js":
-                    res.writeHead(200, {"Content-Type": "text/javascript"});
-                    break;
-                case ".css":
-                    res.writeHead(200, {"Content-Type": "text/css"});
-                    break;
-                case ".gif":
-                    res.writeHead(200, {"Content-Type": "image/gif"});
-                    break;
-                case ".jpg":
-                    res.writeHead(200, {"Content-Type": "image/jpeg"});
-                    break;
-                case ".png":
-                    res.writeHead(200, {"Content-Type": "image/png"});
-                    break;
-                default:
-                    res.writeHead(200, {"Content-Type": "application/octet-stream"});
-            }
-            fs.readFile(pathname,function (err,data){
-                res.end(data);
-            });
-        } else {
-            res.writeHead(404, {"Content-Type": "text/html"});
-            res.end("<h1>404 Not Found</h1>");
-        }
-    }); 
+    var exists = fs.existsSync(pathname);//,function(exists){
+    if(exists){
+		switch(path.extname(pathname)){
+			case ".html":
+			case ".htm":
+				res.writeHead(200, {"Content-Type": "text/html"});
+				break;
+			case ".js":
+				res.writeHead(200, {"Content-Type": "text/javascript"});
+				break;
+			case ".css":
+				res.writeHead(200, {"Content-Type": "text/css"});
+				break;
+			case ".gif":
+				res.writeHead(200, {"Content-Type": "image/gif"});
+				break;
+			case ".jpg":
+				res.writeHead(200, {"Content-Type": "image/jpeg"});
+				break;
+			case ".png":
+				res.writeHead(200, {"Content-Type": "image/png"});
+				break;
+			default:
+				res.writeHead(200, {"Content-Type": "application/octet-stream"});
+		}
+		fs.readFile(pathname,function (err,data){
+			res.end(data);
+		});
+	} else {
+		res.writeHead(404, {"Content-Type": "text/html"});
+		res.end("<h1>404 Not Found</h1>");
+	};
 }
 
 // Loading socket.io
