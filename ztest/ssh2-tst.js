f427ce1e1afabdb5c31f4311bdf20b2775a21a65
lvyv <26896225@qq.com>
2018-03-19 12:48:40 +0800

    format

--- ztest/ssh2-tst.js
+++ ztest/ssh2-tst.js
@@ -6,16 +6,20 @@ let host_config_ = {
     password: '1261123'
     //privateKey: require('fs').readFileSync('./id_dsa')
 };
-
+//test
 //可以工作但到底多少次是成功，多少次失败，
 //在程序中无法控制，这是在封装的类中，没有把错误抛出。
 //进行Promise封装，解决回调地狱问题
 for (let iii = 1; iii <= 50; iii++) {
     console.log("第%d次执行\n", iii);
     let rmt = new ssh2_();
+    doRemoteCmd(rmt);
+};
+
+function doRemoteCmd(rmt) {
     rmt.connect(host_config_, () => {
-        rmt.mkdir("./temp", () => {
-           rmt.disconnect();
+        rmt.mkdir("./temp1", () => {
+            rmt.disconnect();
         });
     });
-};
+}
