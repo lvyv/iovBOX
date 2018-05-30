9e02517c50d7a6768c7aacff9c9fdc35a7cf34a0
yangxianzhi <yangxianzhi@beidouapp.com>
2018-05-30 18:00:45 +0800

    修改led封装

--- demo/led_demo.js
+++ demo/led_demo.js
@@ -1,9 +1,31 @@
-var led= require('../dbus/led.js');
+var dbus_app = require('../dbus/dbus_app.js');
+var ledlib=require('../dbus/led.js');
+var dbus_obj = new dbus_app();
+var led = new ledlib(dbus_obj);
 
-led.setDebugLevel(6,(res)=>{
-    console.log("set debug level "+ res);
+dbus_obj.register_app_name();
+
+led.setDebugLevel(6,function(res){
+    console.log("set debug level "+ JSON.stringify(res));
 });
 
-led.setColor((res) => {
+//color "black blue cyan orange red white pink green"
+led.setColor(1,"black",function(res) {
+    console.log("set color "+ JSON.stringify(res));
+});
 
+led.setColor(2,"black",function(res) {
+    console.log("set color "+ JSON.stringify(res));
+});
+
+led.setColor(3,"black",function(res) {
+    console.log("set color "+ JSON.stringify(res));
+});
+
+led.setColor(4,"black",function(res) {
+    console.log("set color "+ JSON.stringify(res));
+});
+
+led.setColor(0,"red",function(res) {
+    console.log("set color "+ JSON.stringify(res));
 });
